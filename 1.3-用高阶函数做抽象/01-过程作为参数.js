c0541631e8d6147b7e61b1af37fcdb8792e97d0f
shaojie.wang.a <shaojie.wang.a@17zuoye.com>
2022-12-26 15:28:59 +0800

    feat 1.3

--- 1.3-用高阶函数做抽象/01-过程作为参数.js
+++ 1.3-用高阶函数做抽象/01-过程作为参数.js
@@ -20,10 +20,61 @@ function sumCube(a, b) {
     ? 0
     : cube(a) + sumCube(a + 1, b);
 }
-console.log('sumIntegers', sumCube(7, 8));
+console.log('sumCube', sumCube(7, 8));
 
 function piSum(a, b) {
   return a > b
     ? 0
     : (1 / (a * (a + 2)))+ piSum(a + 4, b);
+}
+
+function sum(term, a,  next, b) {
+  return a > b
+    ? 0
+    : term(a) + sum(term, next(a), next, b)
+}
+
+
+
+function inc(num) {
+  return num + 1;
+}
+
+function sumCube1(a, b) {
+  return sum(cube, a, inc, b)
+}
+console.log('sumCube1', sumCube1(1, 10));
+
+function identity(x) {
+  return x;
+}
+
+function sumIntegers1(a, b) {
+  return sum(identity, a, inc, b)
+}
+console.log('sumIntegers1', sumIntegers1(1, 10));
+
+function piSum1(a, b) {
+  function piTerm(x) {
+    return 1 / (x * (x + 2))
+  }
+
+  function piNext(x) {
+    return x + 4;
+  }
+
+  return sum(piTerm, a, piNext, b)
+}
+
+console.log('piSum1', piSum1(1, 1000) * 8);
+
+// 练习1.30
+function sumIter(term, a, next, b) {
+  function iter(result, a) {
+    return a > b
+      ? result
+      : iter(result + term(a), next(a))
+  }
+
+  return iter(0, a)
 }
